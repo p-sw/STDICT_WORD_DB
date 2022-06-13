9716a0ddf9a736b8b4dda3c1dd4d03d9bf3d2f9b
sserve-kr <tube.sserveg@gmail.com>
2022-06-13 16:42:33 +0900

    Fixed things

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 import sqlite3
 import json
 from pprint import pprint
+import logger
+from logger import log
 
 JAMO_START_LETTER = 44032
 JAMO_END_LETTER = 55203
@@ -10,13 +12,19 @@ JAMO_END_LETTER = 55203
 db = sqlite3.connect('word.db')
 cursor = db.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS stdict (word TEXT, length INTEGER)')
+db.commit()
+cursor.execute('DELETE FROM stdict')
+db.commit()
+
+log.info("Initialized.")
 
 APIKEY = os.environ.get("STDICT_APIKEY")
+log.info(f"API KEY: {APIKEY}")
 for charint in range(JAMO_START_LETTER, JAMO_END_LETTER+1):
-    print(f"Trying {chr(charint)}")
+    log.info(f"Trying {chr(charint)}")
     point = 0
     while True:
-        print(f"Trying {point+1}th {chr(charint)}")
+        log.info(f"Trying {point+1}th {chr(charint)}")
         result = requests.get(
             "http://stdict.korean.go.kr/api/search.do",
             params={
@@ -31,25 +39,29 @@ for charint in range(JAMO_START_LETTER, JAMO_END_LETTER+1):
                 "letter_s": 2,
                 "advanced": "y"},
             verify=False)
+        print(result.status_code)
+        print(result.text)
         content = json.loads(result.text)
         pprint(content)
 
         if "error" in content:
             if content['error']['error_code'] == "104":
-                print("invalid start value.")
+                log.warning("Invalid start value.")
                 break
             else:
-                print(content['error']['error_code'], content['error']['message'])
+                log.warning(f"{content['error']['error_code']}: {content['error']['message']}")
 
         history = []
+        if not content:
+            break
 
         for item in content['channel']['item']:
             word = item['word']
-            word = word.replace('-', '')
+            word = word.replace('-', '').replace('ㆍ', '')
             length = len(word)
             if word in history:
                 continue
-            print(word, length)
+            log.info(f"Add {word} | {length}")
             cursor.execute('INSERT INTO stdict (word, length) VALUES (?, ?)', (word, length))
             history.append(word)
         point += 1
